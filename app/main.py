6bbff7c688c67551d5b24063617554c32ae9dbec
MARK CARROLL - STUDENT <g00419267@atu.ie>
2025-11-24 16:34:31 +0000

    Lab 6 B imports

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 
 from app.database import engine, SessionLocal
-from app.models import Base, UserDB
-from app.schemas import UserCreate, UserRead
 
 app = FastAPI(title="Service B - Proxy API")
 SERVICE_A_BASE_URL = os.getenv("SERVICE_A_BASE_URL", "http://localhost:8001")
